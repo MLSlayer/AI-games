17264a23856fd0d99a2ab4289c3f4df94a0cb62d
mlslayer <mlslayergithub@gmail.com>
2019-04-02 17:04:00 -0400

    min max implemented

--- tictactoe/Game.py
+++ tictactoe/Game.py
@@ -4,15 +4,16 @@ from tictactoe.Marks import *
 
 
 class Game(object):
-    def __init__(self):
+    def __init__(self, first_player):
         self.board = -np.ones((3, 3), dtype=int)
-        self.player = X
+        self.player = first_player
         self.moves = dict(enumerate([(i,j) for i in range(3) for j in range(3)]))
         self.valid_moves = self.moves.copy()
+        self.is_tie = False
 
     def make_move(self, move):
         assert move in self.valid_moves
-        assert not self.is_game_over()
+        assert not self.winner_exists()
 
         i, j = self.moves[move]
         self.board[i][j] = self.player
@@ -27,10 +28,13 @@ class Game(object):
         self.player = O if self.player == X else X
 
     def get_winner(self):
-        assert self.is_game_over()
+        assert self.winner_exists()
         return O if self.player == X else X
 
-    def is_game_over(self):
+    def is_over(self):
+        return not self.valid_moves or self.winner_exists()
+
+    def winner_exists(self):
         is_over = any([len(set(row)) <= 1 and EMPTY not in set(row) for row in self.board])
         is_over = any([len(set(col)) <= 1 and EMPTY not in set(col) for col in (self.board.T)]) or is_over
         is_over = len(set(np.diag(self.board))) <= 1 and EMPTY not in set(np.diag(self.board)) or is_over
--- tictactoe/mini-max.py
+++ tictactoe/mini-max.py
@@ -1,31 +1,74 @@
 from tictactoe.Game import *
 import random
 
-x_first = random.random() < .5
-game = Game()
+player_first = random.random() < .5
+game = Game(X if player_first else O)
 
-if (x_first):
+player_dict = {X : "one" if player_first else "two",
+               O : "two" if player_first else "one"}
+
+def minimax(game, max_player):
+    def aux(game, move, depth):
+        if move is not None:
+            game.make_move(move)
+
+        valid_moves = game.valid_moves.copy().keys()
+        if not valid_moves:
+            game.undo_move(move)
+            if depth == 1:
+                return move, (move, 0)
+            return move, 0, True
+
+        if game.winner_exists():
+            winner = game.get_winner()
+            game.undo_move(move)
+            if depth == 1:
+                return move, (move, 1)
+            return (move, 1, True) if winner == max_player else (move, -1, True)
+
+        moves = [aux(game, m, depth + 1) for m in valid_moves]
+        if depth is not 1 and depth is not 0:
+            term_moves = list(map(lambda m : m[1], filter(lambda m: m[2], moves)))
+            moves = list(map(lambda m: m[1], filter(lambda m: not m[2], moves)))
+            moves.append(sum(term_moves))
+            game.undo_move(move)
+            return (move, min(moves), False) if game.player == X else (move, max(moves), False)
+
+        if depth == 0:
+            return max(moves, key = lambda m : m[1][1])
+
+        term_moves = list(map(lambda m : (m[0],m[1]), filter(lambda m: m[2], moves)))
+        moves = list(map(lambda m: (m[0],m[1]), filter(lambda m: not m[2], moves)))
+        moves.extend(term_moves)
+        game.undo_move(move)
+        return move, min(moves, key = lambda m : m[1])
+
+    move = aux(game, None, 0)[0]
+    return move
+
+if (player_first):
     print("valid moves are ", game.valid_moves)
     move = int(input("player one(X), make first move: "))
     game.make_move(move)
-
-player_dict = {1 : "one", 0: "two"}
-
-def minimax(game):
-    return next(iter(game.valid_moves.keys()))
+else:
+    move = minimax(game, O if player_first else X)
+    print("player ", player_dict[game.player], " is now making move ", move)
 
 
-while not game.is_game_over():
-    print("valid moves are ", game.valid_moves)
+while not game.is_over():
     if game.player == X:
+        print("valid moves are ", game.valid_moves)
         move = int(input("player one(X), make move: "))
     else:
-        move = minimax(game)
+        move = minimax(game, O if player_first else X)
         print("player ", player_dict[game.player], " is now making move ", move)
     game.make_move(move)
     print(game)
 
-print("player ", player_dict[game.get_winner()], " wins!")
+if game.winner_exists():
+    print("player ", player_dict[game.get_winner()], " wins!")
+else:
+    print("player one and two ties")
 
 
 
